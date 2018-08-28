0d3ecf1059699d2397242d4da895547cba58635a
Richard Gomer <richard@richardgomer.co.uk>
2018-08-28 11:41:50 +0100

    Working!

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ var sp = document.getElementById('sp'),
     sn = document.getElementById('sn'),
     ck = document.getElementById('ck'),
   sbbt = document.getElementById('submitBtn'),
-  rcbt = document.getElementById('recept-btn');
+  rcbt = document.getElementById('receipt-btn'),
+  btngrp = document.getElementById('btn-group');
+
+btngrp.classList.add('hidden');
 
 //button logic
 sbbt.addEventListener("click", function(event){
@@ -13,19 +16,12 @@ sbbt.addEventListener("click", function(event){
   setTimeout(function (){
     rcbt.classList.add('drop');
     sn.classList.add('hidden');
-    ck.classList.remove('hidden'); 
-    sbbt.classList.add('green')
-  }, 1500); 
+    ck.classList.remove('hidden');
+    sbbt.classList.add('green');
+  }, 1500);
 });
 
-rcbt.addEventListener("click", function(event){
-  event.preventDefault()
-  sbbt.classList.remove('green')
-  sp.classList.remove('hidden')
-  sn.classList.add('hidden');
-  ck.classList.add('hidden'); 
-  rcbt.classList.remove('drop');
-});
+
 
 //Consentua setup
 var ccid = '1'; // Customer ID
@@ -36,16 +32,73 @@ var cuid = false;
 var ccb_ready = function (msg) {
   //When consentua is ready hide load bar
   console.log('UID is:' + msg.message.uid);
-  document.getElementById('loading').classList.add('hidden');
+  //document.getElementById('loading').classList.add('hidden');
   sp.classList.remove('hidden')
   sn.classList.add('hidden');
 };
+
+var got_consent = true;
 var ccb_set = function (msg) {
-  //update cookie when consent is set
   console.log("Consent received from Consentua", msg);
+  got_consent = true;
+  validating = true;
+  validate();
 };
+
+var validating = false;
+function validate(){
+    var ins = [document.getElementById('name'), document.getElementById('email')];
+    var fail = false;
+    for(var i in ins){
+        var inp = ins[i];
+
+        if(inp.value == ""){
+            inp.classList.add('error');
+            fail = true;
+        } else {
+            inp.classList.remove('error');
+        }
+    }
+
+    if(!got_consent || fail){
+        btngrp.classList.add('hidden');
+        return;
+    }
+
+    btngrp.classList.remove('hidden');
+}
+
+// Validate inputs
+var ins = [document.getElementById('name'), document.getElementById('email')];
+var fail = false;
+for(var i in ins){
+    inp = ins[i];
+    inp.addEventListener("change", function(){
+        if(validating){
+            validate();
+        }
+    });
+}
+
 var ciframe = document.getElementById('consentua-cookie-widget');
-var ccwrap = new ConsentuaUIWrapper(ciframe, ccid, cuid, ctid, csid, cskey, function () {} , 'en', {ix: 'https://kni-test-node.herokuapp.com/custom-interaction.html'});
+var ccwrap = new ConsentuaUIWrapper(ciframe, ccid, cuid, ctid, csid, null, function () {} , 'en', {skey: cskey, ix: 'https://kni-test-node.herokuapp.com/custom-interaction.html'});
 // set cb
 ccwrap.onset = ccb_set;
 ccwrap.onready = ccb_ready;
+
+/**
+ * Receipt handling
+ */
+var crurl = false;
+
+rcbt.addEventListener("click", function(event){
+  event.preventDefault();
+
+  if(crurl !== false)
+    window.open(crurl);
+});
+
+ccwrap.onreceipt = function(msg){
+    crurl = msg.message.receiptURL;
+    console.log("Got consent receipt", msg, crurl);
+}
